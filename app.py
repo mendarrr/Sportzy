afb6193fcd17fe85b921bca38538a54dda968a01
mendarrr <sarahabby541@gmail.com>
2024-06-12 19:28:49 +0300

    Implementing the read functionality

--- app.py
+++ app.py
@@ -27,8 +27,10 @@ def main():
 
     user_role = prompt_for_user_role()
     click.echo(f"Hey {user_role} {user_name}")
-    click.echo(f"This is a list of games played in sportzy")
+
+    # Functionality for Player user role
     if user_role == "player":
+        click.echo(f"This is a list of games played in sportzy")
         conn = get_connection()
         cursor = conn.cursor()
         cursor.execute("SELECT game_name FROM games")
@@ -42,10 +44,40 @@ def main():
             click.echo(f"Game Name: {game_details[1]}")
             click.echo(f"Number of Players per team: {game_details[2]}")  
             click.echo(f"Game Coach: {game_details[3]}")
-    else:
-        click.echo("Game not found")
+        else:
+            click.echo("Game not found")
 
-    if user_role == "admin":
+    # Functionality for Coach user role
+    elif user_role == "coach":
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT game_name FROM games")
+        games = cursor.fetchall()
+        for game in games:
+            click.echo(f"{game[0]}")
+        game_name = click.prompt("Which game do you coach?", type=str)
+        cursor.execute("SELECT player_name FROM players WHERE game_name = ?", (game_name,))
+        players = cursor.fetchall()
+        if players:
+            click.echo(f"The list of players you are coaching in {game_name} are:")
+            for player in players:
+                click.echo(f"{player[0]}")
+        else:
+            click.echo(f"No players found for {game_name}")
+        
+        player_name = click.prompt("Which player's details would you like to view?", type=str)
+        cursor.execute("SELECT * FROM players WHERE player_name = ?", (player_name,))
+        player_details = cursor.fetchone()
+        if player_details:
+            click.echo(f"Player Name: {player_details[1]}")
+            click.echo(f"Age: {player_details[5]}")
+            click.echo(f"Gender: {player_details[3]}")
+        else:
+            click.echo(f"The player chosen does not play {game_name}")
+
+
+    # Functionality for admin user role
+    elif user_role == "admin":
         admin_role = prompt_for_admin_role()
         conn = get_connection()
         cursor = conn.cursor()
